dc0c4a64a45b1e8f949cbcae8f61fc5e4a2bf06a
Kaito Fujinuma <sokonashi0124@gmail.com>
2025-01-19 16:46:40 +0900

    add icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,14 @@
 import "@/app/globals.css";
 import { Quicksand } from "next/font/google";
 
+import type { Metadata } from "next";
+
 const quicksand = Quicksand({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "心の健康",
+  description: "Check your mental health level.",
+};
 
 export default function RootLayout({
   children,
@@ -11,6 +18,24 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
         {/*iOS用*/}
         <meta name="apple-mobile-web-app-capable" content="yes" />
         {/*Android用*/}
